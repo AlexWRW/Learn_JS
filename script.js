385e3cadc0887cc10cdbaaaf4a5557f33473af08
Alexis <aleksbumbul@gmail.com>
2022-04-01 00:11:48 +0300

    practicHW2

--- script.js
+++ script.js
@@ -9,12 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('один из последних просмотренных фильмов?', ''),
-      b = +prompt('на сколько его оцените?', ''),
-      c = prompt('один из последних просмотренных фильмов?', ''),
-      d = +prompt('на сколько его оцените?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('один из последних просмотренных фильмов?', ''),
+          b = +prompt('на сколько его оцените?', '');
 
-personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d; 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+        
+}
+
+if (personalMovieDB.count <10) {
+    console.log ('Вы просмотрели довольно мало фильмов');
+} else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
+    console.log ('Вы классический зритель');
+} else if (personalMovieDB.count >=30) {
+    console.log ('Вы киноман!');
+} else {
+    console.log ('ошибка');  
+}
 
 console.log (personalMovieDB);
